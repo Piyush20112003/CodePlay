8a61c6a7ec14a6df29f51805d32c33495704eeb5
Piyush20112003 <piyushpatel20112003@gmail.com>
2025-05-23 19:31:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const router = require('./routes');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
+
+// ✅ Apply limit here BEFORE your routes
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(cookieParser());
+
+// Routes
+app.use('/api', router);
+
+// Server setup
+const PORT = process.env.PORT || 8080;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+        console.log('Connected to MongoDB');
+    });
+});
